d5f5e61fde391f31a20487f628836b77e86fe7ba
paloma-cito <135613515+paloma-cito@users.noreply.github.com>
2023-06-05 16:04:40 +0200

    updated to fit new angular version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,32 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule, Injector} from '@angular/core';
-import {createCustomElement} from '@angular/elements';
-import {EclBuilderComponent} from './ecl-builder/ecl-builder.component';
-import {FormsModule} from '@angular/forms';
-import {NgbTypeaheadModule} from '@ng-bootstrap/ng-bootstrap';
-import {HttpService} from './services/http.service';
-import {HttpClientModule} from '@angular/common/http';
-import {EclService} from './services/ecl.service';
-import {TypeaheadComponent} from './components/typeahead/typeahead.component';
-import {TypeTypeaheadComponent} from './components/type-typeahead/type-typeahead.component';
-import {TargetTypeaheadComponent} from './components/target-typeahead/target-typeahead.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { EclBuilderComponent } from './ecl-builder/ecl-builder.component';
+import { FormsModule } from '@angular/forms';
+import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpService } from './services/http.service';
+import { HttpClientModule } from '@angular/common/http';
+import { EclService } from './services/ecl.service';
+import { TypeaheadComponent } from './components/typeahead/typeahead.component';
+import { TypeTypeaheadComponent } from './components/type-typeahead/type-typeahead.component';
+import { TargetTypeaheadComponent } from './components/target-typeahead/target-typeahead.component';
 
 @NgModule({
-    declarations: [
-        EclBuilderComponent,
-        TypeaheadComponent,
-        TypeTypeaheadComponent,
-        TargetTypeaheadComponent
-    ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        NgbTypeaheadModule,
-        HttpClientModule
-    ],
-    providers: [
-        HttpService,
-        EclService
-    ],
-    entryComponents: [EclBuilderComponent]
+  declarations: [
+    EclBuilderComponent,
+    TypeaheadComponent,
+    TypeTypeaheadComponent,
+    TargetTypeaheadComponent,
+  ],
+  imports: [BrowserModule, FormsModule, NgbTypeaheadModule, HttpClientModule],
+  providers: [HttpService, EclService],
+  //entryComponents: [EclBuilderComponent]
 })
 export class AppModule {
-    constructor(private injector: Injector) {
-        const el = createCustomElement(EclBuilderComponent, {injector});
-        customElements.define('ecl-builder', el);
-    }
+  constructor(private injector: Injector) {
+    const el = createCustomElement(EclBuilderComponent, { injector });
+    customElements.define('ecl-builder', el);
+  }
 
-    ngDoBootstrap(): void {
-
-    }
+  ngDoBootstrap(): void {}
 }
